07dc6311e74393ef174bd00c1ada66538a057d5e
Srini Maddineni <smaddineni@mdstechinc.com>
2016-09-26 08:31:30 -0400

    Changed from Wiki to Google

--- src/test/java/com/mdstech/tadsl/pages/DictionaryPage.java
+++ src/test/java/com/mdstech/tadsl/pages/DictionaryPage.java
@@ -3,7 +3,11 @@ package com.mdstech.tadsl.pages;
 import ch.lambdaj.function.convert.Converter;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import net.serenitybdd.core.pages.WebElementFacade;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
@@ -14,26 +18,37 @@ import java.util.List;
 
 import static ch.lambdaj.Lambda.convert;
 
-@DefaultUrl("http://en.wiktionary.org/wiki/Wiktionary")
+@DefaultUrl("http://google.com")
 public class DictionaryPage extends PageObject {
 
-    @FindBy(name="search")
+    @FindBy(name="q")
     private WebElementFacade searchTerms;
 
-    @FindBy(name="go")
+    @FindBy(name="btnG")
     private WebElementFacade lookupButton;
+    
+    private String keyword;
 
     public void enter_keywords(String keyword) {
+    		this.keyword = keyword;
         searchTerms.type(keyword);
     }
 
     public void lookup_terms() {
-        lookupButton.click();
+//        lookupButton.click();
+        searchTerms.submit();
+	    new WebDriverWait(this.getDriver(), 10).until(new ExpectedCondition<Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver arg0) {
+				return arg0.getTitle().toLowerCase().startsWith(keyword);
+			}
+		});
     }
 
     public List<String> getDefinitions() {
-        WebElementFacade definitionList = find(By.tagName("ol"));
-        List<WebElement> results = definitionList.findElements(By.tagName("li"));
+        WebElementFacade definitionList = find(By.id("ires")).find(By.className("rc"));
+        List<WebElement> results = definitionList.findElements(By.tagName("h3"));
         return convert(results, toStrings());
     }
 
